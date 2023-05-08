3777f2c3a94e9e330bb24c89b8b89773060f848f
franck <franckkoukougnon@yahoo.fr>
2023-05-08 05:04:00 +0200

    methode to delete

--- src/app/composant/pokemon-list/pokemon-list.component.ts
+++ src/app/composant/pokemon-list/pokemon-list.component.ts
@@ -7,33 +7,8 @@ import { Pokemon } from "src/app/models/Pokemon";
   styleUrls: ["./pokemon-list.component.scss"],
 })
 export class PokemonListComponent {
-  @Input() pokemonList: Pokemon[] = [
-    {
-      id: 1,
-      name: "Pikachu",
-      type: "Electric",
-      image: "./assets/images/pikachu.png",
-      shortDescription: "un petit Pokémon Jaune",
-      longDescription:
-        "Pikachu is an Electric-type Pokémon that is very popular among trainers. It is known for its lightning bolt-shaped tail and its ability to generate electricity.",
-    },
-    {
-      id: 2,
-      name: "Charizard",
-      type: "Fire/Flying",
-      image: "./assets/images/Manimal.png",
-      shortDescription: "A large, orange, powerful Pokémon",
-      longDescription:
-        "Charizard is a Fire/Flying-type Pokémon that is known for its ability to breathe fire. It has large wings that allow it to fly and it is very strong in battle.",
-    },
-    {
-      id: 3,
-      name: "Manimal",
-      type: "Varan",
-      shortDescription: "A large, Black, powerful Pokémon",
-      image: "./assets/images/Charizard.png",
-      longDescription:
-        "Manimal is a Varan-type Pokémon that is known for its ability to breathe fire. It has large wings that allow it to fly and it is very strong in battle.",
-    },
-  ];
+  @Input() pokemonAvatar: Pokemon[] = [];
+  onDelect(pokemon: Pokemon) {
+    this.pokemonAvatar = this.pokemonAvatar.filter((p) => p.id !== pokemon.id);
+  }
 }
